779584640a465d7ed1d3d23ae92c88551c9eb90e
mmmries <riesmmm@gmail.com>
2021-01-08 14:51:58 -1000

    add test for querying enums

--- test/proxy_query_field_test.exs
+++ test/proxy_query_field_test.exs
@@ -16,6 +16,16 @@ defmodule Absinthe.Compose.ProxyQueryFieldTest do
         meta(compose: Downstream.pong())
         resolve(&Absinthe.Compose.resolve/3)
       end
+
+      field :fruits, list_of(:fruit) do
+        meta(compose: Downstream.produce())
+        resolve(&Absinthe.Compose.resolve/3)
+      end
+
+      field :vegetables, list_of(:vegetable) do
+        meta(compose: Downstream.produce())
+        resolve(&Absinthe.Compose.resolve/3)
+      end
     end
 
     object :paddle do
@@ -27,6 +37,30 @@ defmodule Absinthe.Compose.ProxyQueryFieldTest do
       field(:name, :string)
       field(:key, :string)
       field(:favorite_paddle, :paddle)
+    end
+
+    enum :produce_color do
+      value(:red)
+      value(:orange)
+      value(:yellow)
+      value(:green)
+    end
+
+    enum :vitamin do
+      value(:c)
+      value(:d)
+    end
+
+    object :fruit do
+      field(:name, non_null(:string))
+      field(:color, :produce_color)
+      field(:sweetness, :integer)
+    end
+
+    object :vegetable do
+      field(:name, non_null(:string))
+      field(:color, :produce_color)
+      field(:primary_vitamin, :vitamin)
     end
   end
 
@@ -86,5 +120,27 @@ defmodule Absinthe.Compose.ProxyQueryFieldTest do
     assert {:ok, %{data: data}} = Absinthe.run(@player_query, Schema, variables: variables)
 
     assert data == %{"player" => nil}
+  end
+
+  test "querying enum values from downstream" do
+    query = """
+    query {
+      fruits {
+        name
+        color
+        sweetness
+      }
+    }
+    """
+
+    assert {:ok, %{data: data}} = Absinthe.run(query, Schema)
+
+    assert data == %{
+             "fruits" => [
+               %{"color" => "YELLOW", "name" => "banana", "sweetness" => 4},
+               %{"color" => "RED", "name" => "tomato", "sweetness" => 1},
+               %{"color" => "GREEN", "name" => "watermelon", "sweetness" => 7}
+             ]
+           }
   end
 end
