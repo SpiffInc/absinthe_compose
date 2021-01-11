e5cf3fcb524a9b9bdabd000bc3357fc4bdb1430d
mmmries <riesmmm@gmail.com>
2021-01-11 10:10:42 -1000

    prepare for first release

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # AbsintheCompose
 
 Compose multiple GraphQL endpoints into a single unified graph.
+
+## Getting Started
+
+The simplest way to compose GraphQL endpoints is to act as a gateway where you forward requests for your different top-level queries to different backends.
+
+```elixir
+defmodule Schema do
+  use Absinthe.Schema
+
+  query do
+    field :paddles, list_of(:paddle) do
+      meta(
+        compose: [
+          from: Absinthe.Compose.HTTPClient,
+          opts: [
+            url: "http://localhost:9000/graphql"
+          ]
+        ]
+      )
+      resolve(&Absinthe.Compose.resolve/3)
+    end
+  end
+
+  # object/field definitions go here
+end
+```
+
+We add a little meta-data to the field so and tell Absinthe to have this library resolve it.
+Then `absinthe_compose` will call `Absinthe.Compose.HTTPClient` with the opts we've provided, a GraphQL query of all the fields requested under the `paddles` root node and any variables we need to pass along.
+
+For more examples, see `test/proxy_query_field_test.exs` and `test/proxy_mutation_field_test.exs`.
--- mix.exs
+++ mix.exs
@@ -5,9 +5,10 @@ defmodule AbsintheCompose.MixProject do
     [
       app: :absinthe_compose,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -26,6 +27,7 @@ defmodule AbsintheCompose.MixProject do
       {:httpoison, "~> 1.7"},
       # dev/test dependencies
       {:absinthe_plug, "~> 1.5", only: [:dev, :test]},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
       {:jason, "~> 1.0", only: [:dev, :test]},
       {:plug_cowboy, "~> 2.0", only: [:dev, :test]}
     ]
@@ -33,4 +35,17 @@ defmodule AbsintheCompose.MixProject do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      maintainers: ["Michael Ries"],
+      licenses: ["MIT"],
+      description: """
+      An Absinthe extension for composing GraphQL endpoints together
+      """,
+      links: %{
+        github: "https://github.com/SpiffInc/absinthe_compose"
+      },
+    ]
+  end
 end
