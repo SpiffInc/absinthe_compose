37c4482bd56c08960b7b81f3977b7457cddc7be1
Filipe Froes <fr.froes@gmail.com>
2021-01-25 14:06:35 +0200

    fix: typo

--- test/plug/forward_headers_test.exs
+++ test/plug/forward_headers_test.exs
@@ -5,11 +5,11 @@ defmodule Absinthe.Compose.Plug.ForwardHeaderTest do
   alias Absinthe.Compose.Plug.ForwardHeader
 
   setup do
-    ForwardHeader.init(["Auhtorization", "origin"])
+    ForwardHeader.init(["authorization", "origin"])
 
     conn =
       conn("post", "/", "")
-      |> put_req_header("auhtorization", "some-token")
+      |> put_req_header("authorization", "some-token")
       |> put_req_header("origin", "www.coolwebsite.com")
       |> put_req_header("header-to-ignore", "ignored")
 
@@ -19,17 +19,17 @@ defmodule Absinthe.Compose.Plug.ForwardHeaderTest do
   end
 
   test "fowards given headers to abshinte context", %{conn: conn} do
-    conn = ForwardHeader.call(conn, ["auhtorization", "Origin"])
+    conn = ForwardHeader.call(conn, ["authorization", "Origin"])
     assert %{context: %{headers_to_forward: headers_to_forward}} = conn.private[:absinthe]
 
-    assert [{"origin", ["www.coolwebsite.com"]}, {"auhtorization", ["some-token"]}] =
+    assert [{"origin", ["www.coolwebsite.com"]}, {"authorization", ["some-token"]}] =
              headers_to_forward
 
     assert length(headers_to_forward) == 2
   end
 
   test "ignores non-existent headers", %{conn: conn} do
-    conn = ForwardHeader.call(conn, ["auhtorization", "origin", "some-giberish"])
+    conn = ForwardHeader.call(conn, ["authorization", "origin", "some-giberish"])
     assert %{context: %{headers_to_forward: headers_to_forward}} = conn.private[:absinthe]
     assert length(headers_to_forward) == 2
   end
